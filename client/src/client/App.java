62e489e99cec39d468d6712a3de685b3dc0182e2
Gregory Eric Sanderson <gzou2000@gmail.com>
2011-09-29 13:00:21 -0400

    moved client into its own folder

---
+++ client/src/client/App.java
@@ -0,0 +1,131 @@
+package client;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+
+import server.Server;
+import server.ServerInterface;
+
+public class App {
+	
+	final public static String 	DEFAULT_HOST = "localhost";
+	final public static int 	DEFAULT_PORT = 2222;
+	final public static String 	PROGRAM_NAME = "Cartes500";
+	
+	private static ServerInterface server = null;
+	private static Registry registry = null;
+	
+	private static CommandLine parseArgs(String[] args) {
+		
+		Options options = new Options();
+
+		Option port = OptionBuilder.withLongOpt("port")
+									.withArgName("PORT")
+									.hasArg()
+									.withDescription("Port of the rmi registry server")
+									.create("p");
+		
+		Option host = OptionBuilder.withLongOpt("host")
+									.withArgName("HOST")
+									.hasArg()
+									.withDescription( "Address of the rmi registry server, a.k.a the game server" )
+									.create("H");
+		
+		
+		options.addOption("h", "help", false, "print this help message");
+		options.addOption("c", "console", false, "start console app");
+		options.addOption("g", "graphic", false, "start graphical app");
+		options.addOption( host );
+		options.addOption( port );
+										
+		CommandLineParser parser = new PosixParser();
+		
+		CommandLine cmd = null;
+		
+		try{
+			cmd = parser.parse( options, args );
+		} catch (ParseException e) {
+			System.out.println("ERROR: cannot parse arguments.");
+			System.out.println( e.getMessage() );
+		}
+		
+		if( cmd != null && cmd.hasOption("help") ) {
+		
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp( App.PROGRAM_NAME , options );
+			return null;
+
+		}
+
+		//So Greg is Greg...
+		return cmd;
+	}
+	
+	private static boolean connectToServer( String host, int port ) {
+		
+		App.server = new Server();
+			
+		try {
+			
+			System.out.println("Connecting to RMI Registry...");
+			App.registry = LocateRegistry.getRegistry( host, port );
+			App.server = (ServerInterface)registry.lookup( App.PROGRAM_NAME );
+			
+		} catch (RemoteException e) {
+			System.out.println("Error during connection to server. Stacktrace:");
+			e.printStackTrace();
+			return false;
+		} catch (NotBoundException e) {
+			System.out.println("Error during connection to server. Stacktrace:");
+			e.printStackTrace();
+			return false;
+		}
+		
+		System.out.println("Connected.");
+		
+		return true;
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		CommandLine cmd = null;
+		String host = App.DEFAULT_HOST;
+		int port = App.DEFAULT_PORT;
+		
+		cmd = App.parseArgs( args );
+		
+		if( cmd != null ) {
+			
+			if ( cmd.hasOption("host") ) {
+				host = cmd.getOptionValue("host");
+			}
+			
+			if ( cmd.hasOption("port") ) {
+				port = Integer.parseInt( cmd.getOptionValue("port") );
+			}
+			
+			System.out.println(host);
+			System.out.println(port);
+			
+			//App.connectToServer( host, port );
+			
+		}
+
+	}
+	
+}
