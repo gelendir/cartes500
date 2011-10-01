38ff3464db3081bcbf90be2b3281a81b3a80a6e8
Gregory Eric Sanderson <gzou2000@gmail.com>
2011-10-01 17:48:30 -0400

    console: show players' cards, ask for a card to play

--- client/src/view/AbstractView.java
+++ client/src/view/AbstractView.java
@@ -1,7 +1,9 @@
 package view;
 
 import server.Server;
+import game.Card;
 import game.Game;
+import game.Hand;
 import game.Player;
 
 public abstract class AbstractView {
@@ -28,5 +30,9 @@ public abstract class AbstractView {
 	protected Player getPlayer() {
 		return this.player;
 	}
-
+	
+	public abstract Card getCardToPlay( Hand hand );
+	
+	public abstract void showPlayerTurn( Player player, Card card );
+	
 }
--- client/src/view/ConsoleView.java
+++ client/src/view/ConsoleView.java
@@ -1,32 +1,39 @@
 package view;
 
 import java.io.PrintStream;
+import java.text.MessageFormat;
 import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 import java.util.Scanner;
 
+import game.Card;
 import game.Game;
+import game.Hand;
 import game.Player;
 import server.Server;
+import server.Turn;
 
 public class ConsoleView extends AbstractView {
+	
+	final private static String BUNDLE = "console";
 	
 	protected Scanner in;
 	protected PrintStream out;
 	protected PropertyResourceBundle bundle;
 	
-	public ConsoleView( Server server, Game game, Player player ) {
+	private ConsoleView( Server server, Game game, Player player, 
+			Scanner in, PrintStream out, PropertyResourceBundle bundle ) {
 		
 		super( server, game, player );
-		this.in = new Scanner( System.in );
-		this.out = System.out;
-		this.bundle = (PropertyResourceBundle) ResourceBundle.getBundle("console");
+		this.in = in;
+		this.out = out;
+		this.bundle = bundle;
 		
 	}
 	
 	public static ConsoleView welcome( Server server, Game game ) {
 		
-		PropertyResourceBundle bundle = (PropertyResourceBundle) ResourceBundle.getBundle("console");
+		PropertyResourceBundle bundle = (PropertyResourceBundle) ResourceBundle.getBundle( ConsoleView.BUNDLE );
 		PrintStream out = System.out;
 		Scanner in = new Scanner( System.in );
 		
@@ -39,7 +46,60 @@ public class ConsoleView extends AbstractView {
 		
 		out.println( player );
 		
-		return new ConsoleView( server, game, player );
+		return new ConsoleView( server, game, player, in, out, bundle );
 	}
 	
+	public void printTurn( Turn turn ) {
+		
+		this.out.println( this.bundle.getString("currentGameBoardBanner") );
+		this.out.println( turn.toString() );
+		
+	}
+	
+	public void printHand( Hand hand ) {
+		
+		Card[] cards = hand.getCards();
+		Card card = null;
+		
+		String cardTemplate = this.bundle.getString("cardSelectionTemplate");
+		String cardLine = "";
+		
+		this.out.println( this.bundle.getString("currentHandBanner") );
+		
+		for( int i = 0; i < cards.length; ++i ) {
+			
+			card = cards[i];
+			cardLine = MessageFormat.format( cardTemplate, Integer.toString( i ), card.toString() );
+			this.out.println( cardLine );
+					
+		}
+		
+	}
+	
+	public Card getCardToPlay( Hand hand ) {
+		
+		int cardNumber = -1;
+		
+		this.printHand( hand );
+		
+		while( cardNumber < 0 ) {
+			this.out.println( this.bundle.getString("playerSelectCard") );
+			cardNumber = this.in.nextInt();
+		}
+		
+		Card card = hand.getCards()[ cardNumber ];
+		
+		return card;
+
+	}
+	
+	public void showPlayerTurn( Player player, Card card ) {
+		
+		String template = this.bundle.getString("playerTurn");
+		String result = MessageFormat.format( template, player.toString(), card.toString() );
+		
+		this.out.println( result );
+		
+	}
+
 }
