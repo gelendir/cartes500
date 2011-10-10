b3b520435aeb1ad361d4402a89a389e26d75cb69
Gregory Eric Sanderson <gzou2000@gmail.com>
2011-10-10 18:33:42 -0400

    Client : refactored to use new client interface

--- client/src/client/Client.java
+++ client/src/client/Client.java
@@ -1,32 +1,58 @@
 package client;
 
+import game.Bet;
 import game.Card;
+import game.Hand;
 import game.Player;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
+import server.Server;
 import server.Turn;
+import view.AbstractView;
 
 public class Client implements ClientInterface {
+	
+	private Server server;
+	private AbstractView view;
+	private Player player;
+	private ArrayList<Player> players;
 
-	@Override
-	public void startNewGame(ArrayList<Card> cards, ArrayList<Player> players)
-			throws RemoteException {
-		// TODO Auto-generated method stub
+	public Client( Server server, AbstractView view ) throws RemoteException {
+		
+		this.server = server;
+		this.view = view;
+		this.players = null;
+		
+		this.view.welcome();
+		this.player = this.view.createPlayer();
+		this.server.connectClient( this , this.player );
+		
+	}
 
+	public Player getPlayer() {
+		return player;
 	}
 
 	@Override
-	public void notifyYourTurn() throws RemoteException {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public Turn notifyTurn(Turn turn) throws RemoteException {
-		// TODO Auto-generated method stub
-		return null;
+	public void notifyTurn(Turn turn) throws RemoteException {
+		
+		this.view.showPlayerTurn( turn );
+		Player lastPlayer = turn.getLatestPlayer();
+		
+		int index = this.players.indexOf( lastPlayer );
+		
+		if( 
+				( index + 1 >= this.players.size() && this.players.get(0).equals( this.player ) ) ||
+				( this.players.get( index + 1 ).equals( this.player ) )
+		) {
+			
+			Card card = this.view.getCardToPlay( this.player.getHand() );
+			this.server.playCard( this, card );
+			
+		}
+		
 	}
 
 	@Override
@@ -45,6 +71,46 @@ public class Client implements ClientInterface {
 	public void notifyExit() throws RemoteException {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void notifyPlayerConnect(Player player) throws RemoteException {
+		
+		this.view.playerConnected( player );	
+		
+	}
+
+	@Override
+	public Bet notifyBettingTime( Hand hand ) throws RemoteException {
+		
+		this.player.setHand( hand );
+		Bet bet = this.view.askBet( hand );
+		return bet;
+		
+	}
+
+
+	@Override
+	public void notifyPlayerBet(Player player, Bet bet) {
+		
+		this.view.playerHasBet( player, bet );
+		
+		
+	}
+
+	@Override
+	public void notifyStartNewGame(ArrayList<Player> players)
+			throws RemoteException {
+		
+		this.players = players;
+		
+		if( this.players.get(0).equals( this.player ) ) {
+			
+			Card card = this.view.getCardToPlay( this.player.getHand() );
+			this.server.playCard( this, card );
+			
+		}
+		
 	}
 
 }
