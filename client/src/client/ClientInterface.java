8d4058823f42732ca38f37c6dcbd1a5531f95012
Gregory Eric Sanderson <gzou2000@gmail.com>
2011-10-10 18:33:17 -0400

    ClientInterface : refactored events

--- client/src/client/ClientInterface.java
+++ client/src/client/ClientInterface.java
@@ -1,6 +1,7 @@
 package client;
 
-import game.Card;
+import game.Bet;
+import game.Hand;
 import game.Player;
 
 import java.rmi.Remote;
@@ -11,16 +12,20 @@ import server.Turn;
 
 public interface ClientInterface extends Remote {
 	
-	public void startNewGame( ArrayList<Card> cards, ArrayList<Player> players ) throws RemoteException;
+	public void notifyStartNewGame( ArrayList<Player> players ) throws RemoteException;
 	
-	public void notifyYourTurn() throws RemoteException;
-	
-	public Turn notifyTurn( Turn turn ) throws RemoteException;
+	public void notifyTurn( Turn turn ) throws RemoteException;
 	
 	public void notifyPlayerDisconnect( Player player ) throws RemoteException;
+	
+	public void notifyPlayerConnect( Player player ) throws RemoteException;
+	
+	public Bet notifyBettingTime( Hand hand ) throws RemoteException;
 	
 	public void notifyWinner( Player player ) throws RemoteException;
 	
 	public void notifyExit() throws RemoteException;
+	
+	public void notifyPlayerBet( Player player, Bet bet );
 
 }
