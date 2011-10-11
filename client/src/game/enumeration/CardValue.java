a94b0167555bfed06b942e3294a56b41ebeb9d4a
Gregory Eric Sanderson <gzou2000@gmail.com>
2011-10-11 16:16:11 -0400

    Reorder cards to put ace at the top

--- client/src/game/enumeration/CardValue.java
+++ client/src/game/enumeration/CardValue.java
@@ -1,19 +1,19 @@
 package game.enumeration;
 
 public enum CardValue {
-	ACE (1),
-	TWO (2),
-	THREE (3),
-	FOUR (4),
-	FIVE (5),
-	SIX (6),
-	SEVEN (7),
-	EIGHT (8),
-	NINE (9),
-	TEN (10),
-	JACK (11),
-	QUEEN (12),
-	KING (13),
+	TWO (1),
+	THREE (2),
+	FOUR (3),
+	FIVE (4),
+	SIX (5),
+	SEVEN (6),
+	EIGHT (7),
+	NINE (8),
+	TEN (9),
+	JACK (10),
+	QUEEN (11),
+	KING (12),
+	ACE (13),
 	JOKER (14);
 	
 	private final int value;
