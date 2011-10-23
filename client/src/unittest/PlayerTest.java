ea2fbe0e26786519b20bd69a64a4994dd73f05e9
sidarape <fredy_14@live.fr>
2011-10-23 18:05:27 -0400

    Création de la classe de test de Player.

---
+++ client/src/unittest/PlayerTest.java
@@ -0,0 +1,39 @@
+package unittest;
+
+import junit.framework.TestCase;
+
+public class PlayerTest extends TestCase {
+
+	public final void testPlayer() {
+		fail("Not yet implemented");
+	}
+
+	public final void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	public final void testSetOriginalBet() {
+		fail("Not yet implemented");
+	}
+
+	public final void testGetOriginalBet() {
+		fail("Not yet implemented");
+	}
+
+	public final void testSetHand() {
+		fail("Not yet implemented");
+	}
+
+	public final void testGetHand() {
+		fail("Not yet implemented");
+	}
+
+	public final void testGetTurnWin() {
+		fail("Not yet implemented");
+	}
+
+	public final void testAddTurnWin() {
+		fail("Not yet implemented");
+	}
+
+}
