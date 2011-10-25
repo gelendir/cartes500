298152b9d6170bb9dd1734329ac83d9968a36c06
Gregory Eric Sanderson <gzou2000@gmail.com>
2011-10-24 21:27:53 -0400

    add unit tests for CardComparator

---
+++ client/src/unittest/CardComparatorTest.java
@@ -0,0 +1,77 @@
+package unittest;
+
+import junit.framework.TestCase;
+
+import game.Card;
+import game.CardComparator;
+import game.enumeration.Suit;
+import game.enumeration.CardValue;
+
+public class CardComparatorTest extends TestCase {
+
+	private CardComparator c;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		this.c = new CardComparator();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testSmallCardHighCard() throws Exception  {
+		
+		Card low = new Card( Suit.NONE, CardValue.FOUR );
+		Card high = new Card( Suit.NONE, CardValue.EIGHT );
+		
+		assertEquals( this.c.compare( low, high ), -1 );
+		
+	}
+	
+	public void testHighCardSmallCard() throws Exception {
+		
+		Card low = new Card( Suit.NONE, CardValue.FOUR );
+		Card high = new Card( Suit.NONE, CardValue.NINE );
+		
+		assertEquals( this.c.compare( high, low ), 1 );
+		
+	}
+	
+	public void testSameCardDifferentSuit() throws Exception {
+		
+		Card card1 = new Card( Suit.CLUBS, CardValue.SIX );
+		Card card2 = new Card( Suit.HEARTS, CardValue.SIX );
+		
+		assertEquals( this.c.compare( card1, card2 ), 0 );
+		
+	}
+	
+	public void testAceHigherThanFour() throws Exception {
+		
+		Card ace = new Card( Suit.DIAMONDS, CardValue.ACE );
+		Card four = new Card( Suit.SPADES, CardValue.FOUR );
+		
+		assertEquals( this.c.compare( ace, four ), 1 );
+	}
+	
+	public void testJokerHigherThanFour() throws Exception {
+		
+		Card joker = new Card( Suit.COLOR, CardValue.JOKER );
+		Card four = new Card( Suit.SPADES, CardValue.FOUR );
+		
+		assertEquals( this.c.compare( joker, four ), 1 );
+		
+	}
+	
+	public void testJokerHigherThanAce() throws Exception {
+		
+		Card joker = new Card( Suit.COLOR, CardValue.JOKER );
+		Card ace = new Card( Suit.SPADES, CardValue.ACE );
+		
+		assertEquals( this.c.compare( joker, ace ), 1 );
+		
+	}
+
+
+}
