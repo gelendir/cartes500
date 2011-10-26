f47baca8d28ed6e68e3e4938bec1d41c8ecb46ec
Fredy <Fredy@Fredy-Laptop>
2011-10-26 12:16:53 -0400

    Javadoc de Card

--- client/src/game/Card.java
+++ client/src/game/Card.java
@@ -3,11 +3,31 @@ package game;
 import game.enumeration.CardValue;
 import game.enumeration.Suit;
 
+/**
+ * La classe Card représente une carte de jeu.
+ * @author Frédérik Paradis
+ * */
 public class Card {
+	
+	/**
+	 * La sorte de la carte
+	 */
 	private final Suit suit;
+	
+	/**
+	 * La valeur de la carte
+	 */
 	private final CardValue value;
 	
-	public Card(Suit suit, CardValue value) throws Exception { //throws Exception {
+	/**
+	 * Le constructeur de Card crée une nouvelle carte avec la sorte et
+	 * la valeur spécifié. Une exception est levée lorsque l'on crée une carte
+	 * qui n'existe pas. Par exemple, un Joker de Pique lèverait une exception.
+	 * @param suit La sorte de la carte
+	 * @param value La valeur de la carte 
+	 * @throws Exception
+	 */
+	public Card(Suit suit, CardValue value) throws Exception {
 		if(value.equals(CardValue.JOKER)) {
 			if(!suit.equals(Suit.BLACK) && !suit.equals(Suit.COLOR)) {
 				throw new Exception("Playing Card does not exist.");
@@ -21,10 +41,18 @@ public class Card {
 		this.value = value;
 	}
 	
+	/**
+	 * Cette méthode retourne la sorte de la carte.
+	 * @return Retourne la sorte de la carte
+	 */
 	public Suit getSuit() {
 		return this.suit;
 	}
 	
+	/**
+	 * Cette méthode retourne la valeur de la carte.
+	 * @return Retourne la valeur de la carte
+	 */
 	public CardValue getCardValue() {
 		return this.value;
 	}
