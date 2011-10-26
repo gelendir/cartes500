d8a3a7dcd3df9fe8b3631521b03b670b6551147e
Gregory Eric Sanderson <gzou2000@gmail.com>
2011-10-26 18:48:58 -0400

    Java doc for Client

--- client/src/client/Client.java
+++ client/src/client/Client.java
@@ -11,18 +11,59 @@ import java.util.ArrayList;
 
 import server.Server;
 import view.AbstractView;
-
+/**
+ * Client du jeu de 500. La classe Client est en charge de se
+ * connecter au serveur de jeu, d'agir en tant qu'intermédiaire entre
+ * le serveur et le joueur humain et d'agir comme controleur pour l'interface
+ * (console ou graphique) du jeu. La classe Client communique avec le serveur
+ * en utilisant le patron de conception "Observeur Observé". Le client observe le serveur,
+ * et reçoit des notifications d'événements lors des divers actions dans le jeu.
+ * Les fonctions événements de la classe client sont préfixés avec le mot "notify".
+ * 
+ * @see ClientInterface
+ * @author Gregory Eric Sanderson <gzou2000@gmail.com>
+ *
+ */
 public class Client implements ClientInterface {
 	
+	/**
+	 * Le serveur auquel ce client est connecté
+	 */
 	private Server server;
+	
+	/**
+	 * La vue (console ou graphique) à utiliser
+	 */
 	private AbstractView view;
+	
+	/**
+	 * Le profil du joueur géré par ce client
+	 */
 	private Player player;
+	
+	/**
+	 * La liste des joueurs participant au jeu.
+	 */
 	private ArrayList<Player> players;
 	
+	/**
+	 * Le gagnant de la mise.
+	 */
 	private Player betWinner = null;
 	
+	/**
+	 * L'atout du jeu
+	 * @See game.enumeration.Suit
+	 */
 	private Suit gameSuit;
 	
+	/**
+	 * Constructeur. Crée une nouvelle instance du client.
+	 * 
+	 * @param server Le serveur du jeu.
+	 * @param view La vue à utiliser pour interagir avec le joueur.
+	 * @throws RemoteException Erreurs RMI.
+	 */
 	public Client( Server server, AbstractView view ) throws RemoteException {
 		
 		this.server = server;
@@ -34,14 +75,33 @@ public class Client implements ClientInterface {
 		
 	}
 	
+	/**
+	 * Fonction appelé lorsque le client est prêt à se connecter au serveur.
+	 * Le client s'enregistrera au près du serveur à travers RMI.
+	 * 
+	 * @return Si la connexion a réussi ou échoué.
+	 * @throws RemoteException Erreurs RMI.
+	 */
 	public boolean connect() throws RemoteException {
 		return this.server.connectClient( this, this.player );
 	}
 
+	/**
+	 * Accesseur. Retourne le profil du joueur assoicé à ce client.
+	 * 
+	 * @return Le joueur.
+	 */
 	public Player getPlayer() {
 		return player;
 	}
 	
+	/**
+	 * Notification du tour d'un joueur.
+	 * Veuillez vous référer à la documentation de la classe ClientInterface
+	 * pour plus de détails.
+	 * 
+	 * @see ClientInterface#notifyPlayerTurn(Player, Card)
+	 */
 	@Override
 	public void notifyPlayerTurn( Player player, Card card )
 			throws RemoteException {
@@ -50,6 +110,13 @@ public class Client implements ClientInterface {
 		
 	}
 
+	/**
+	 * Notification du tour au joueur courant.
+	 * Veuillez vous référer à la documentation de la classe ClientInterface
+	 * pour plus de détails.
+	 * 
+	 * @see ClientInterface#notifyYourTurn(Suit)
+	 */
 	@Override
 	public Card notifyYourTurn( Suit suit ) {
 		
@@ -59,24 +126,52 @@ public class Client implements ClientInterface {
 
 	}
 
+	/**
+	 * Notification de la déconnexion d'un joueur
+	 * Veuillez vous référer à la documentation de la classe ClientInterface
+	 * pour plus de détails.
+	 * 
+	 * @see ClientInterface#notifyPlayerDisconnect(Player)
+	 */
 	@Override
 	public void notifyPlayerDisconnect(Player player) throws RemoteException {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Notification du gagnant du jeu.
+	 * Veuillez vous référer à la documentation de la classe ClientInterface
+	 * pour plus de détails.
+	 * 
+	 * @see ClientInterface#notifyWinner(Player, Player)
+	 */
 	@Override
 	public void notifyWinner(Player player, Player player2) throws RemoteException {
 		this.view.showWinners( player, player2 );
 
 	}
 
+	/**
+	 * Notification de fermeture du serveur.
+	 * Veuillez vous référer à la documentation de la classe ClientInterface
+	 * pour plus de détails.
+	 * 
+	 * @see ClientInterface#notifyExit()
+	 */
 	@Override
 	public void notifyExit() throws RemoteException {
 		// TODO Auto-generated method stub
 
 	}
 
+	/**
+	 * Notification de la connexion d'un joueur.
+	 * Veuillez vous référer à la documentation de la classe ClientInterface
+	 * pour plus de détails.
+	 * 
+	 * @see ClientInterface#notifyPlayerConnect(Player)
+	 */
 	@Override
 	public void notifyPlayerConnect(Player player) throws RemoteException {
 		
@@ -84,6 +179,13 @@ public class Client implements ClientInterface {
 		
 	}
 
+	/**
+	 * Notification de la période de mise.
+	 * Veuillez vous référer à la documentation de la classe ClientInterface
+	 * pour plus de détails.
+	 * 
+	 * @see ClientInterface#notifyBettingTime(Hand)
+	 */
 	@Override
 	public Bet notifyBettingTime( Hand hand ) throws RemoteException {
 		
@@ -93,7 +195,13 @@ public class Client implements ClientInterface {
 		
 	}
 
-
+	/**
+	 * Notification de la mise d'un joueur.
+	 * Veuillez vous référer à la documentation de la classe ClientInterface
+	 * pour plus de détails.
+	 * 
+	 * @see ClientInterface#notifyPlayerBet(Player, Bet)
+	 */
 	@Override
 	public void notifyPlayerBet(Player player, Bet bet) {
 		
@@ -102,6 +210,13 @@ public class Client implements ClientInterface {
 		
 	}
 
+	/**
+	 * Notification du démarrage d'une nouvelle partie.
+	 * Veuillez vous référer à la documentation de la classe ClientInterface
+	 * pour plus de détails.
+	 * 
+	 * @see ClientInterface#notifyStartNewGame(ArrayList)
+	 */
 	@Override
 	public void notifyStartNewGame(ArrayList<Player> players)
 			throws RemoteException {
@@ -111,6 +226,13 @@ public class Client implements ClientInterface {
 		
 	}
 
+	/**
+	 * Notification de nouvelles cartes après avoir gagné une mise.
+	 * Veuillez vous référer à la documentation de la classe ClientInterface
+	 * pour plus de détails.
+	 * 
+	 * @see ClientInterface#notifyChangeCardsAfterBet(Card[])
+	 */
 	@Override
 	public void notifyChangeCardsAfterBet( Card[] newCards ) {
 		
@@ -119,6 +241,13 @@ public class Client implements ClientInterface {
 		
 	}
 
+	/**
+	 * Notification du gagnant de la mise.
+	 * Veuillez vous référer à la documentation de la classe ClientInterface
+	 * pour plus de détails.
+	 * 
+	 * @see ClientInterface#notifyBetWinner(Player, Suit)
+	 */
 	@Override
 	public void notifyBetWinner(Player player, Suit gameSuit) {
 		this.gameSuit = gameSuit;
@@ -126,6 +255,13 @@ public class Client implements ClientInterface {
 		this.view.showBetWinner( player, gameSuit );
 	}
 
+	/**
+	 * Notification du gagnant du tour.
+	 * Veuillez vous référer à la documentation de la classe ClientInterface
+	 * pour plus de détails.
+	 * 
+	 * @see ClientInterface#notifyTurnWinner(Player)
+	 */
 	@Override
 	public void notifyTurnWinner(Player player) {
 		this.view.showTurnWinner( player );
