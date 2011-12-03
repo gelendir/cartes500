35d360dec52ad48fe4f19228eebd18e8bf7936fd
Gregory Eric Sanderson <gzou2000@gmail.com>
2011-12-03 15:31:46 -0500

    update of invalid bet exception

--- client/src/exception/InvalidBetException.java
+++ client/src/exception/InvalidBetException.java
@@ -1,6 +1,10 @@
 package exception;
 
 public class InvalidBetException extends GameException {
+	
+	public InvalidBetException() {
+		super("The bet is invalid");
+	}
 
 	public InvalidBetException(String message) {
 		super(message);
