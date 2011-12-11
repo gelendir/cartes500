a36b4552ef11e57c5c7868a40100ca0a55ea8244
Frédérik Paradis <fredy_14@live.fr>
2011-12-11 18:03:04 -0500

    Finition de la gestion de la base de données, il ne manque que le débogage.

--- server/src/database/PlayerStatitics.java
+++ server/src/database/PlayerStatitics.java
@@ -1,16 +1,26 @@
 package database;
 
+import game.Bet;
+
 public class PlayerStatitics {
 	private int idPlayer;
 	private String name;
 	private int nbPlayedGame;
 	private int nbWonGame;
 	private int totalPoints;
-	
-	public PlayerStatitics() {
-		
+
+	PlayerStatitics(int idPlayer, 
+			String name, 
+			int nbPlayedGame, 
+			int nbWonGame, 
+			int totalPoints) {
+		this.idPlayer = idPlayer;
+		this.name = name;
+		this.nbPlayedGame = nbPlayedGame;
+		this.nbWonGame = nbWonGame;
+		this.totalPoints = totalPoints;
 	}
-	
+
 	public int getIdPlayer() {
 		return idPlayer;
 	}
@@ -18,16 +28,20 @@ public class PlayerStatitics {
 	public String getName() {
 		return name;
 	}
-	
+
 	public void addAPlayedGame() {
 		++this.nbPlayedGame;
 	}
-	
-	public void addAWonGame(int points) {
+
+	public void addAWonGame(Bet bet) {
+		int points = 40;
+		points += bet.getSuit().getValue() * 20 - 20;
+		points += (bet.getNbRounds() - 6) * 100;
+		
 		++this.nbWonGame;
 		this.totalPoints += points;
 	}
-	
+
 	public int getNbPlayedGame() {
 		return this.nbPlayedGame;
 	}
--- server/src/database/Statistics.java
+++ server/src/database/Statistics.java
@@ -1,6 +1,10 @@
 package database;
 
+import game.Player;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -11,10 +15,24 @@ public class Statistics {
 			"CREATE TABLE IF NOT EXISTS PLAYER (" +
 					"	ID_PLAYER INTEGER PRIMARY KEY NOT NULL, " +
 					"	NAME TEXT NOT NULL, " +
-					"	NB_PLAYED_GAME INTEGER NOT NULL, " +
-					"	NB_WON_GAME INTEGER NOT NULL, " +
-					"	TOTAL_POINTS INTEGER NOT NULL" +
+					"	NB_PLAYED_GAME INTEGER NOT NULL SET DEFAULT 0, " +
+					"	NB_WON_GAME INTEGER NOT NULL SET DEFAULT 0, " +
+					"	TOTAL_POINTS INTEGER NOT NULL SET DEFAULT 0" +
 					");";
+	private static final String GET_PLAYER_SQL_QUERY = 
+			"SELECT" +
+					"	ID_PLAYER, " +
+					"	NAME, " +
+					"	NB_PLAYED_GAME, " +
+					"	NB_WON_GAME, " +
+					"	TOTAL_POINTS" +
+					"FROM " +
+					"	PLAYER" +
+					"WHERE" +
+					"	PLAYER.NAME = ?";
+
+	private static final String INSERT_PLAYER_SQL_QUERY = 
+			"INSERT INTO PLAYER (NAME) VALUES (?);";
 	
 	private Connection conn;
 
@@ -24,5 +42,52 @@ public class Statistics {
 		Statement stat = conn.createStatement();
 		stat.executeUpdate(Statistics.CREATE_TABLE_PLAYER);
 		stat.close();
+	}
+
+	public PlayerStatitics getPlayer(Player player) throws SQLException {
+		return this.getPlayer(player.getName());
+	}
+
+	public PlayerStatitics getPlayer(String name) throws SQLException {
+		PlayerStatitics retour;
+		
+		PreparedStatement prep = this.conn.prepareStatement(Statistics.GET_PLAYER_SQL_QUERY);
+		prep.setString(1, name);
+		ResultSet result = prep.executeQuery();
+		
+		prep.getGeneratedKeys();
+
+		if(result.next()) {
+			int idPlayer = result.getInt("ID_PLAYER");
+			name = result.getString("NAME");
+			int nbPlayedGame = result.getInt("NB_PLAYED_GAME");
+			int nbWonGame = result.getInt("NB_WON_GAME");
+			int totalPoints = result.getInt("TOTAL_POINTS");
+			
+			retour = new PlayerStatitics(
+					idPlayer, 
+					name, 
+					nbPlayedGame, 
+					nbWonGame, 
+					totalPoints);
+		}
+		else {
+			PreparedStatement insert = this.conn.prepareStatement(Statistics.INSERT_PLAYER_SQL_QUERY);
+			insert.setString(1, name);
+			ResultSet lastID = insert.getGeneratedKeys();
+			int lastInsertID = 0;
+			if(lastID.next()) {
+				lastInsertID = lastID.getInt(1);
+			}
+			
+			retour = new PlayerStatitics(lastInsertID, name, 0,	0, 0);
+			
+			insert.close();
+		}
+		
+		result.close();
+		prep.close();
+		
+		return retour;
 	}
 }
