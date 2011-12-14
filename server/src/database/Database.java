c46b83139a49b9e922f21599423b993e8a8f590b
Frédérik Paradis <fredy_14@live.fr>
2011-12-13 19:04:09 -0500

    javadoc

--- server/src/database/Database.java
+++ server/src/database/Database.java
@@ -6,15 +6,39 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Cette classe gère les connexions à la BD. Le patron de conception
+ * de cette classe est un Singleton multiple permettant d'avoir plusieurs
+ * connexions à différentes base de données.
+ * @author Frédérik Paradis
+ */
 public class Database {
 
+	/**
+	 * La chaine de connexion à la base de données.
+	 */
 	private static final String JDBC_DRIVER = "org.sqlite.JDBC";
+	
+	/**
+	 * Le Connection String de la base de données.
+	 */
 	private static final String CONNECTION_STRING = "jdbc:sqlite:%s";
 
+	/**
+	 * Association entre les noms de BD et leur connexion.
+	 */
 	private Map<String, Connection> connections = new HashMap<String, Connection>();
 
+	/**
+	 * L'instance unique de Database.
+	 */
 	private static Database instance = null;
 
+	/**
+	 * Cette méthode retourne l'instance unique de Database.
+	 * @return Retourne l'instance unique de Database.
+	 * @throws ClassNotFoundException
+	 */
 	private static Database getInstance() throws ClassNotFoundException {
 		if(Database.instance == null) {
 			Database.instance = new Database();
@@ -22,10 +46,22 @@ public class Database {
 		return Database.instance;
 	}
 
+	/**
+	 * Cette méthode retourne la connexion d'une base de données.
+	 * @param database Le nom de la BD
+	 * @return Retourne la connexion d'une base de données.
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
 	public static Connection getConnection(String database) throws ClassNotFoundException, SQLException {
 		return Database.getInstance().getOrCreateConnection(database);
 	}
 
+	/**
+	 * Cette méthode ferme et supprime une connexion à une base de
+	 * données.
+	 * @param database Le nom de la BD à fermer.
+	 */
 	public static void deleteConnection(String database) {
 		try {
 			Database.getInstance().removeConnection(database);
@@ -35,10 +71,20 @@ public class Database {
 		}
 	}
 
+	/**
+	 * Ce constructeur initialise le pilote JDBC.
+	 * @throws ClassNotFoundException
+	 */
 	private Database() throws ClassNotFoundException {
 		Class.forName(Database.JDBC_DRIVER);
 	}
 
+	/**
+	 * Cette méthode retourne la connexion d'une base de données.
+	 * @param database Le nom de la BD
+	 * @return Retourne la connexion d'une base de données.
+	 * @throws SQLException
+	 */
 	private Connection getOrCreateConnection(String database) throws SQLException {
 		if(!this.connections.containsKey(database)) {
 
@@ -55,6 +101,11 @@ public class Database {
 		return this.connections.get(database);
 	}
 
+	/**
+	 * Cette méthode ferme et supprime une connexion à une base de
+	 * données.
+	 * @param database Le nom de la BD à fermer.
+	 */
 	private void removeConnection(String database) {
 		if(this.connections.containsKey(database)) {
 			try {
@@ -70,6 +121,9 @@ public class Database {
 
 	}
 
+	/**
+	 * Le destructeur ferme toutes les connexions de base de données.
+	 */
 	@Override
 	protected void finalize() {
 		for(Connection c : connections.values()) {
